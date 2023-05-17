b60dcb5e8ec48d7d300dc9505d403689e7abd9e4
luclucyt <88875@glr.nl>
2023-05-17 13:28:58 +0200

    commit

--- PHP copy/agenda/index.php
+++ PHP copy/agenda/index.php
@@ -30,185 +30,14 @@
 <body>
     <?php include '../forceLogin.php' ?>
     <?php include '../connectDatabase.php'; ?>
-    <?php include '../connectCustomDB.php'; ?>
     <?php include 'addToAgenda.php'; ?>
-    
-    <header class="main-header">
-        <div class="header-left-side">
-            <h2 class="header-welcome">Welkom <?= $_SESSION['username'] ?>,</h2>
-
-            <div class="header-current-week">
-                <?php displayWeekDateHeader(); ?>
-            </div>
-
-            <form method="post" class="week-button-wrapper">
-                <div class="change-week-wrapper">
-                    <input type="submit" name="prev_week" value="<" class="change-week-button">
-                    <input type="submit" name="next_week" value=">" class="change-week-button">
-                </div>
-                <input type="submit" name="this_week" value="Vandaag" class="this-week">
-            </form>
-
-            <!-- change week --> 
-            <?php include 'changeWeek.php'; ?>
-        </div>
-
-        
-        <form method="post" action="" class="header-right-side">
-            <input type="submit" name="logout" value="Log uit" class="header-log-out">
-            <input type="submit" name="settings-input" value="Instellingen" class="header-settings-input" onclick="window.location.href = '../settings.php';">
-            <input type="button" name="share-input" value="Deel" class="header-share-input">
-        </form>
-    </header>
-
-    <!-- share form -->
-    <div class="share-display-wrapper">
-        <div class="share-display-from-wrapper">
-            <form action="" method="POST" autocomplete="off">
-                <input type="text" placeholder="Gebruikersnaam" class="share-display-form-input" name="share-main-form">
-                
-                <div id="result"></div>
-                
-                <input type="submit" value="Deel" class="share-display-form-submit" name="share-from-submit">
-            </form>
-        </div>
-    </div>
-
-    <?php
-        if(isset($_POST['shareInputValue'])){
-            $shareInputValue = $_POST['shareInputValue'];
-            echo "<div class='reponesForHTML'>";
-                if($shareInputValue !== "") {
-                    $SQL =  "SELECT * FROM login WHERE username LIKE '%$shareInputValue%'";
-                    $resultShare = mysqli_query($connection, $SQL);
-                    $resultCheck = mysqli_num_rows($resultShare);
-
-                    if ($resultCheck > 0) {
-                        while ($row = mysqli_fetch_assoc($resultShare)) {
-                            $shareUserID = $row['username'];
-                            echo "<div class='share-user-wrapper' onclick='updateShare(`$shareUserID`)'><p class='share-user'>$shareUserID</p></div>";
-                        }
-                    }
-                }
-            echo "</div>";
-        }
-    ?>
-
-    <section class="view-wrapper">
-        <div class="functie-wrapper">
-            <div class="new-color-wrapper">
-                <h4>Nieuwe functie toevoegen:</h4>
-                <form method="POST" action="index.php">
-                    <label for="new-functie">Nieuwe functie:</label>
-                    <input type="text" id="new-functie" name="new-functie" placeholder="Nieuwe functie"><br>
-
-                    <label for="new-color">Nieuwe kleur:</label>
-                    <input type="text" data-coloris class="coloris instance1" id="new-color" name="new-color" value="#77077d"><br>
-
-                    <input type="hidden" name="userID" value="<?=  $userID ?>">
-
-                    <input type="submit" name="new-color-submit" value="Toevoegen" class="new-color-submit"><br><br>
-                </form>
-            </div>
-
-            <div class="functie-line"></div>
-            
-            <div class="remove-color-wrapper">
-                <h4>Functie verwijderen:</h4>
-                <form method="POST" action="">
-                    <input type="hidden" name="userID" value="<?=  $userID ?>">
-
-                    <label for="remove-functie">Verwijder functie:</label><br>
-                    <select name="remove-color-select" id="remove-color-select">
-                        <?php getColors($userID, $connection); ?>
-                    </select><br>
-                    <input type="submit" name="remove-color-submit" value="Verwijderen" class="remove-color-submit">
-                </form>
-            </div>
-            <?php 
-                //if color is deleted
-                if(isset($_POST['remove-color-select'])){
-                    $id = $_POST['remove-color-select'];
-
-                    //get the data from the database
-                    $sql = "SELECT * FROM kleuren WHERE userID = '{$userID}' AND kleur = '{$id}'";
-                    $result = mysqli_query($connection, $sql);
-
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $colorID = $row['id'];
-                        $colorUserID = $row['userID'];
-                        $colorName = $row['kleur'];
-                        $colorFunction = $row['functie'];
-
-                        //delete the data from the database
-                        removeFromCustomDB("kleuren", $colorID, $colorUserID, $colorName, $colorFunction);
-                    }
-
-                    $SQL = "DELETE FROM kleuren WHERE userID = '{$userID}' AND kleur = '{$id}'";
-
-                    $result = mysqli_query($connection, $SQL);
-
-                    echo "<script>window.location.href</script>";
-                    echo "<script>window.location.reload()</script>";
-                } 
-            ?>
-        </div>
-
-        <div class="agenda-filter-wrapper">
-            <div class="filter-wrapper">
-                <h4>Filter:</h4>
-                <form method="POST" action="">
-                    <select name="filter-functie" id="filter-functie">
-                        <option value="0">Geen filter</option>
-                        <?php getColors($userID, $connection) ?>
-                    </select><br>
-                </form>
-            </div>
-        </div>
-
-        <div class="agenda-hide-wrapper"></div>
-    </section>
-
     
 
     
     <div class="main-main-agenda-wrapper">
-        <div class='agenda-header'>
-            <?php
-                for ($i = 0; $i < 7; $i++) {
-                    $date = date('jS M', strtotime($_SESSION['week_start'] . ' +' . $i . ' days'));
+       
 
-                    // Get the name of the day of the week
-                    $day_name = date('l', strtotime($_SESSION['week_start'] . ' +' . $i . ' days'));
-
-
-                    if    ($day_name == "Monday"   ) { $day_name = "Ma";}
-                    elseif($day_name == "Tuesday"  ) { $day_name = "Di";} 
-                    elseif($day_name == "Wednesday") { $day_name = "Wo";} 
-                    elseif($day_name == "Thursday" ) { $day_name = "Do";} 
-                    elseif($day_name == "Friday"   ) { $day_name = "Vr";} 
-                    elseif($day_name == "Saturday" ) { $day_name = "Za";}
-                    elseif($day_name == "Sunday"   ) { $day_name = "Zo";}
-
-
-                    //if it is today, add the class 'current-day'
-                    if ($date == date('jS M')) {
-                        $date = "<div class='agenda-day current-day'><label style='font-size:1vw'>$date $day_name</label></div>";
-                    } else {
-                        $date = "<div class='agenda-day'><label style='font-size:1vw'>$date $day_name</label></div>";
-                    }
-                    echo $date;
-                }
-            ?>
-        </div>
-
-        <div class='agenda-line-wrapper'>
-            <?php
-                for($i = 1; $i <= 7; $i++){
-                    echo "<div class='agenda-day-line'></div>";
-                }
-            ?>
-        </div>
+   
       
         <div class='agenda-grid-wrapper'>
              
@@ -216,14 +45,14 @@
                 <?php 
                     for($i = 0; $i <= 23; $i++){ 
                     echo '<div class="time-wrapper">
-                            <!-- <div class="time-header"><label>'.$i.' uur</label></div> -->
+                            <div class="time-header"><label>'.$i.' uur</label></div>
                             <div class="time-line"></div>
                         </div> ';
                     }
                 ?>
             </div>
                     
-            <?php require_once 'displayAgendaItems.php'; ?>
+            
         </div>
     </div>
 </body>
@@ -233,7 +62,7 @@
 <script>
     let agenda_wrapper = document.getElementsByClassName('agenda-grid-wrapper')[0];
     let start_timeInverted = false;
-    let row_amount = 92;
+    let row_amount = 96;
     let colom_amount = 7;
 
     let is_dragging = false;
@@ -253,31 +82,28 @@
 
     agenda_wrapper.addEventListener('mousedown', function(event) {
 
-    if (event.target === agenda_wrapper) {
-        //mouse is pressed on the agenda
-        is_dragging = true;
-        start_row = get_row(event)[0] + 1;
-        start_time = get_row(event)[1];
+        if (event.target === agenda_wrapper) {
+            //mouse is pressed on the agenda
+            is_dragging = true;
+            start_row = get_row(event)[0];
+            start_time = get_row(event)[1];
 
-        colom = get_colom(event)[0] + 1;
+            colom = get_colom(event)[0] + 1;
 
-        week_start = document.getElementById('week_start').value;
-        week_start = new Date(week_start);
+            week_start = document.getElementById('week_start').value;
+            week_start = new Date(week_start);
 
-        start_date = new Date(week_start);
-        start_date.setDate(week_start.getDate() + get_colom(event)[1]);
+            start_date = new Date(week_start);
+            start_date.setDate(week_start.getDate() + get_colom(event)[1]);
 
-        start_date = start_date.toISOString().substring(0, 10)
+            start_date = start_date.toISOString().substring(0, 10)
 
-        //document.getElementById('agenda-start-time').value = start_time;
-
-
-        //remove all the temp agenda items
-        let temp_items = document.querySelectorAll('.agenda-item-temp');
-        temp_items.forEach(function (item) {
-            item.remove();
-        });
-    }
+            //remove all the temp agenda items
+            let temp_items = document.querySelectorAll('.agenda-item-temp');
+            temp_items.forEach(function (item) {
+                item.remove();
+            });
+        }
 
     });
 
@@ -314,6 +140,15 @@
 
                 start_timeInverted = true;
             }
+
+            //if start/end time is bigger than 24:00 then change it to 23:45
+            if (start_time >= 23.75){
+                start_time = 23.75;
+            }
+            if (end_time >= 23.76){
+                end_time = 23.75;
+            }
+
 
             document.getElementsByClassName('agenda-item-temp')[0].innerHTML = `
             <form method="POST" action="" autocomplete="off" id="add-to-agenda-form">
@@ -404,11 +239,7 @@
     function get_row(event){
         let rect = agenda_wrapper.getBoundingClientRect();
         let y = event.clientY - rect.top;
-<<<<<<< Updated upstream
-        let row_height = agenda_wrapper.clientHeight / row_amount;
-=======
         row_height = 14;
->>>>>>> Stashed changes
 
         //calucate the time that corresponds to the row (1 row = 15 minutes)
         let time = Math.floor(y / row_height) * 15;
@@ -418,7 +249,7 @@
         // format the time value so it can be used in the input field
         let formatted_time = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
 
-        //console.log(formatted_time);
+       
 
         return [Math.floor(y / row_height), formatted_time];
     }
